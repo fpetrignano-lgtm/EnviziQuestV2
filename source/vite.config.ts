f1cd5e7c77de1d31cbbc696a6b74ae976d1e80c6
Felice Petrignano <fpetrignano@gmail.com>
2026-08-30 15:14:44 +0200

    feat: add P10slide slideshow from blank1, add p10-slide-1..7 assets

--- source/vite.config.ts
+++ source/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: "/EnviziQuestV2/",
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/EnviziQuestV2/" : "/",
   plugins: [react()],
-});
+}));
